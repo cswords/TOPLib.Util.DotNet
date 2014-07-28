c2312f2adb8972cc74c170c429e1db69c5434148
cswords <barry.cswords@gmail.com>
2014-07-28 11:43:04 +1000

    New version:)
    may not be good

--- TOPLib.Util.DotNet.Persistence/Db/BOO.cs
+++ TOPLib.Util.DotNet.Persistence/Db/BOO.cs
@@ -1,40 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.IO;
-using System.Data;
 
 namespace TOPLib.Util.DotNet.Persistence.Db
 {
     public static class BOO
     {
-        public static Constraint L(string exp)
+        public static Constraint L(string t)
         {
-            var result = new SingleConstraint(exp);
+            return new SingleConstraint(t);
+        }
+
+        public static IDictionary<string, string> M(params string[] fields)
+        {
+            var result = new Dictionary<string, string>();
+            foreach (var field in fields)
+            {
+                if (result.ContainsKey(field))
+                    result[field] = field;
+                else
+                    result.Add(field, field);
+            }
             return result;
         }
 
-        public static Constraint L(this object o, string exp)
+        public static string Indentation(this string source, string space = "\t")
         {
-            return L(exp);
+            return space + source.Replace("\n", "\n" + space);
         }
 
-        public static Constraint M(string left, object right)
+        public static IEnumerable<IDictionary<string, object>> ToLocalData(this DataTable dataTable)
         {
-            return L(left + " = '" + right.ToString() + "'");
-        }
-
-        public static Constraint M(this object o, string left, object right)
-        {
-            return L(left + " = '" + right.ToString() + "'");
-        }
-
-        public static bool ImportCSV(this BAM db, FileInfo file, string table, string timeStampField, string rowIdField, string filenameField)
-        {
-            if (db.DbType == TOPLib.Util.DotNet.Persistence.Db.BAM.DataBaseType.MsSql)
+            var result = new LinkedList<IDictionary<string, object>>();
+            if (dataTable != null)
             {
-                var exist = db.DetectTable(table);
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    var rowDict = new Dictionary<string, object>();
+                    foreach (DataColumn column in dataTable.Columns)
+                    {
+                        rowDict[column.ColumnName] = row[column.ColumnName];
+                    }
+                    result.AddLast(rowDict);
+                }
+            }
+            return result;
+        }
+
+        public static IEnumerable<IRowSchema> GetSchema(this IExtractable query)
+        {
+            var schemaTable = ((JointBase)query).Context.GetSchemaTable(query.ToSQL());
+            var result = new LinkedList<IRowSchema>();
+            foreach (DataRow schemaRow in schemaTable.Rows)
+            {
+                var row = new RowSchema(schemaRow);
+                if (!row.FieldName.Equals("__ROWID__"))
+                    result.AddLast(row);
+            }
+            if (query is AbstractSelectQuery)
+            {
+                foreach (var f in ((AbstractSelectQuery)query).tohide)
+                {
+                    var s = result.SingleOrDefault(r => r.FieldName == f);
+                    if (s != null) result.Remove(s);
+                }
+            }
+            return result;
+        }
+
+        public static bool ImportCSV(this IDatabase db, FileInfo file, string table, string timeStampField, string rowIdField, string filenameField)
+        {
+            if (db is MsSQLDb)
+            {
+                var exist = ((Bamboo)db).DetectTable(table);
                 var sql = string.Empty;
                 //sql += "EXEC master.dbo.sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.12.0' , N'DynamicParameters' , 1;";
                 if (exist)
@@ -49,12 +91,12 @@ namespace TOPLib.Util.DotNet.Persistence.Db
                 sql += " FROM OPENDATASOURCE ('Microsoft.ACE.OLEDB.12.0', 'Data Source=";
                 sql += file.DirectoryName;
                 sql += ";Extended Properties=\"Text;HDR=Yes;FMT=Delimited\"' )...[" + file.Name.Replace(".", "#") + "];";
-                return db.Execute(sql);
+                return ((Bamboo)db).Execute(sql);
             }
             else throw new NotSupportedException("MySQL is not supported yet.");
         }
 
-        public static string ToCSV(this DataTable table, string separator=",", string lineSeparator="\n")
+        public static string ToCSV(this DataTable table, string separator = ",", string lineSeparator = "\r\n")
         {
             string result = string.Empty;
             //header
---
+++ TOPLib.Util.DotNet.Persistence/Db/Joint.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TOPLib.Util.DotNet.Persistence.Db
+{
+    internal abstract class JointBase : ISql
+    {
+        public Bamboo Context { get; internal set; }
+
+        public Joint LowerJoint { get; internal set; }
+
+        public abstract string ToSQL();
+    }
+
+    internal abstract class Joint : JointBase
+    {
+        internal T CreateUpper<T>()
+            where T : JointBase, new()
+        {
+            var upper = new T();
+
+            upper.Context = this.Context;
+            upper.LowerJoint = this;
+
+            return upper;
+        }
+    }
+
+    internal class QueryBase : Joint, IQueryBase
+    {
+        public override string ToSQL()
+        {
+            return this.LowerJoint.ToSQL();
+        }
+
+        public IQuery Where(Constraint constraint)
+        {
+            var result = this.CreateUpper<Query>();
+            result.WhereClaus = constraint;
+            return result;
+        }
+
+        public IQuery All
+        {
+            get
+            {
+                var result = this.CreateUpper<Query>();
+                result.WhereClaus = null;
+                return result;
+            }
+        }
+    }
+
+    internal class Query : Joint, IQuery
+    {
+        public Constraint WhereClaus { get; internal set; }
+
+        public ISelectable Select
+        {
+            get
+            {
+                var result = this.CreateUpper<SelectHelper>();
+                return result;
+            }
+        }
+
+        public IGroupable GroupBy
+        {
+            get
+            {
+                var result = this.CreateUpper<GroupByHelper>();
+                return result;
+            }
+        }
+
+        public ISortable OrderBy
+        {
+            get
+            {
+                var result = this.CreateUpper<OrderByHelper>();
+                return result;
+            }
+        }
+
+        public IWriteOnly ToUpdate(string name)
+        {
+            var result = this.CreateUpper<UpdateQuery>();
+            result.target = name;
+            return result;
+        }
+
+        public IExecutable ToDelete
+        {
+            get
+            {
+                var result = this.CreateUpper<DeleteQuery>();
+                return result;
+            }
+        }
+
+        public override string ToSQL()
+        {
+            if (WhereClaus == null)
+                return LowerJoint.ToSQL();
+            else
+                return LowerJoint.ToSQL() + "\nWHERE " + WhereClaus.ToString();
+        }
+    }
+
+    internal class Table : QueryBase, ITable
+    {
+        internal Table(string name)
+        {
+            this.Name = name;
+        }
+
+        public override string ToSQL()
+        {
+            string result = "FROM ";
+            if (Name.StartsWith(Context.LeftBracket) & Name.EndsWith(Context.RightBracket))
+                result += Name;
+            else
+                result += Context.LeftBracket + Name + Context.RightBracket;
+            return result;
+        }
+
+        public string Name
+        {
+            get;
+            private set;
+        }
+
+        public IDictionary<string, Type> Schema
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public IWriteOnly ToInsert
+        {
+            get
+            {
+                var result = this.CreateUpper<InsertQuery>();
+                return result;
+            }
+        }
+
+        public IAliasedTable As(string alias)
+        {
+            var result = this.CreateUpper<AliasedTable>();
+            result.Alias = alias;
+            return result;
+        }
+    }
+
+    internal class JoinableQueryBase : QueryBase, IJoinable
+    {
+
+        public IJoinning<IAliasedLeftJoinning> LeftJoin(IRight query)
+        {
+            var result = this.CreateUpper<Joinning<AliasedLeftJoinning, IAliasedLeftJoinning>>();
+            result.Right = query;
+            return result;
+        }
+
+        public IJoinning<IAliasedInnerJoinning> InnerJoin(IRight query)
+        {
+            var result = this.CreateUpper<Joinning<AliasedInnerJoinning, IAliasedInnerJoinning>>();
+            result.Right = query;
+            return result;
+        }
+
+        public IJoinning<IAliasedCrossJoinning> CrossJoin(IRight query)
+        {
+            var result = this.CreateUpper<Joinning<AliasedCrossJoinning, IAliasedCrossJoinning>>();
+            result.Right = query;
+            return result;
+        }
+
+        public IJoinning<IAliasedLeftJoinning> LeftJoin(string tableName)
+        {
+            return LeftJoin(Context[tableName]);
+        }
+
+        public IJoinning<IAliasedInnerJoinning> InnerJoin(string tableName)
+        {
+            return InnerJoin(Context[tableName]);
+        }
+
+        public IJoinning<IAliasedCrossJoinning> CrossJoin(string tableName)
+        {
+            return CrossJoin(Context[tableName]);
+        }
+    }
+
+    internal class AliasedTable : JoinableQueryBase, IAliasedTable
+    {
+        public string Alias { get; set; }
+
+        public override string ToSQL()
+        {
+            return LowerJoint.ToSQL() + " AS " + Context.LeftBracket + Alias + Context.RightBracket;
+        }
+    }
+
+    internal class Joinning<T, I> : Joint, IJoinning<I>
+        where I : IAliasedJoinning<I>
+        where T : Joint, I, IAliasedJoinning<T>, new()
+    {
+        public IRight Right { get; internal set; }
+
+        public override string ToSQL()
+        {
+            var result = LowerJoint.ToSQL();
+
+            if (typeof(T) == typeof(AliasedLeftJoinning))
+                result += "\nLEFT JOIN ";
+            if (typeof(T) == typeof(AliasedInnerJoinning))
+                result += "\nINNER JOIN ";
+            if (typeof(T) == typeof(AliasedCrossJoinning))
+                result += "\nCROSS JOIN ";
+
+            if (this.Right is ITable)
+                result += this.Context.LeftBracket + ((ITable)this.Right).Name + this.Context.RightBracket;
+            else
+                result += "(\n" + this.Right.ToSQL().Indentation() + "\n)";
+
+            return result;
+        }
+
+        public I As(string alias)
+        {
+            var result = this.CreateUpper<T>();
+            result.Alias = alias;
+            return result;
+        }
+    }
+
+    internal abstract class AbstractAliasedJoinning<T> : Joint, IAliasedJoinning<T>
+    {
+        public string Alias { get; set; }
+
+        public override string ToSQL()
+        {
+            return LowerJoint.ToSQL() + " AS " + Context.LeftBracket + Alias + Context.RightBracket;
+        }
+    }
+
+    internal abstract class AbstractJoinQueryBase<T> : AbstractAliasedJoinning<T>, IJoinQueryBase<T>
+        where T : AbstractJoinQueryBase<T>, new()
+    {
+        public IJoinQuery On(Constraint constraint)
+        {
+            var result = this.CreateUpper<JoinQuery>();
+            result.OnClause = constraint;
+            return result;
+        }
+
+        public override string ToSQL()
+        {
+            return LowerJoint.ToSQL() + " AS " + Context.LeftBracket + Alias + Context.RightBracket;
+        }
+    }
+
+    internal class AliasedLeftJoinning : AbstractJoinQueryBase<AliasedLeftJoinning>, IAliasedLeftJoinning { }
+
+    internal class AliasedInnerJoinning : AbstractJoinQueryBase<AliasedInnerJoinning>, IAliasedInnerJoinning { }
+
+    internal class JoinQuery : QueryBase, IJoinQuery
+    {
+        public Constraint OnClause { get; internal set; }
+
+        public override string ToSQL()
+        {
+            var result = LowerJoint.ToSQL();
+            if (OnClause != null)
+                result += " ON " + OnClause.ToString();
+            return result;
+        }
+    }
+
+    internal class AliasedCrossJoinning : JoinQuery, IAliasedCrossJoinning, IAliasedJoinning<AliasedCrossJoinning>
+    {
+        public string Alias { get; set; }
+
+        public override string ToSQL()
+        {
+            return LowerJoint.ToSQL() + " AS " + Context.LeftBracket + Alias + Context.RightBracket;
+        }
+    }
+}
